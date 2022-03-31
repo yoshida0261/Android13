234a5febee851bee8d98c6e74fd54d26f9bdc1ad
yoshida261 <yoshida261@gmail.com>
2022-04-01 05:36:00 +0900

    Add Photo picker

--- app/src/main/java/com/stah/android13/MainActivity.kt
+++ app/src/main/java/com/stah/android13/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.stah.android13
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.stah.android13.ui.theme.Android13Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,19 +22,30 @@ class MainActivity : ComponentActivity() {
             Android13Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+
+                    color = MaterialTheme.colors.primary
                 ) {
                     Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val activity = LocalContext.current as Activity
+    TextButton(onClick = { requestPicker(activity = activity) }) {
+        "Hello $name!"
+    }
+}
+
+fun requestPicker(activity: Activity) {
+    val PHOTO_PICKER_REQUEST_CODE = 1
+    val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
+    ActivityCompat.startActivityForResult(activity, intent, PHOTO_PICKER_REQUEST_CODE, null)
 }
 
 @Preview(showBackground = true)
